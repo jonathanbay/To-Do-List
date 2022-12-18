72b5701bfd22b8e4b9ffb0d00ff2159383632f46
jonathan <bayart.jonathan@laposte.net>
2022-12-18 20:47:01 +0100

    mise en place du local storage

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const form = document.querySelector("form");
 
+// Local storage
+function storeList() {
+  window.localStorage.todolist = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todolist) {
+    list.innerHTML = window.localStorage.todolist;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+}
+// On peut appeller la fonction directement
+// getTodos();
+
+// Ou faire jouer la fonction au chargement de la page => bonne pratique
+window.addEventListener("load", () => {
+  getTodos();
+});
+
 // Add element
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   list.innerHTML += `<li>${item.value}</li>`;
   item.value = "";
+  storeList();
 });
 
 // Remove element
@@ -15,4 +36,5 @@ list.addEventListener("click", (e) => {
   } else {
     e.target.classList.add("checked");
   }
+  storeList();
 });
